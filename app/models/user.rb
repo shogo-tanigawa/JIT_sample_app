7c04f2c7109a671539a33b73f7e196e31b801955
shogo-tanigawa <kurohamu2525@gmail.com>
2023-04-30 17:41:11 +0900

    edit画面の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, length: { minimum: 6 }
 end
