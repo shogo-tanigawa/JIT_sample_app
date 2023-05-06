b2eb70c337513254d46f6120aa66b9771a7b5ddb
shogo-tanigawa <kurohamu2525@gmail.com>
2023-05-06 18:01:33 +0900

    dash_boardsを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/dash_boards'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root 'application#hello'
   
   devise_for :users
+  resources :users, only: [:dash_boards]
+  get 'users/dash_boards', to: 'users#dash_boards', as: 'user_dashboard'
 
   devise_scope :users do
     get '/users', to: redirect("/users/sign_up")
