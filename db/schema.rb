b37c6973fe3094e4fce17813494b71354bad1ce5
shogo-tanigawa <kurohamu2525@gmail.com>
2023-05-13 00:13:14 +0900

    articleテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_30_073844) do
+ActiveRecord::Schema.define(version: 2023_05_12_150102) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title", limit: 10, null: false
+    t.text "content", null: false
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
