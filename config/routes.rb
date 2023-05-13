85fc1d5a0abfadd4000f84e2003fb85d2575d462
shogo-tanigawa <kurohamu2525@gmail.com>
2023-05-14 01:16:09 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/dash_boards'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root 'users#dash_boards'
   
   devise_for :users
-  resources :users, only: [:dash_boards]
+  resources :users do
+    resources :articles
+  end
   get 'users/dash_boards', to: 'users#dash_boards', as: 'user_dashboard'
 
   devise_scope :users do
